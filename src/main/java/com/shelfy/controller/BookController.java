7d9e42c5879b9d51f22ecfce29337ef315a1d8fa
ekkang2 <rkddmsrud27@gmail.com>
2025-02-05 12:30:05 +0900

    responseDTO 수정

--- src/main/java/com/shelfy/controller/BookController.java
+++ src/main/java/com/shelfy/controller/BookController.java
@@ -7,6 +7,7 @@ import com.shelfy.service.AladinService;
 import com.shelfy.service.BookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,11 +38,17 @@ public class BookController {
      */
     @GetMapping("/search")
     public ResponseDTO<List<BookDTO>> searchBooks(@RequestParam String query) {
-
         log.info("책 검색 query : " + query);
+
         List<BookDTO> bookDTOList = bookService.searchBooks(query);
         log.info("책 검색 반환 bookDTOList : " + bookDTOList);
 
+        // 검색 결과가 없으면 204 No Content 반환
+        if (bookDTOList.isEmpty()) {
+            return new ResponseDTO<>(true, bookDTOList, HttpStatus.NO_CONTENT.value(), null);
+        }
+
+        // 검색 성공 (200 OK)
         return ResponseDTO.success(bookDTOList);
     }
 
--- src/main/java/com/shelfy/dto/ResponseDTO.java
+++ src/main/java/com/shelfy/dto/ResponseDTO.java
@@ -3,6 +3,7 @@ package com.shelfy.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Data
 @NoArgsConstructor
@@ -18,12 +19,12 @@ public class ResponseDTO<T> {
 
     // 성공 응답을 위한 static 메서드
     public static <T> ResponseDTO<T> success(T response) {
-        return new ResponseDTO<>(true, response, 200, null);
+        return new ResponseDTO<>(true, response, HttpStatus.OK.value(), null);
     }
 
     // 실패 응답을 위한 static 메서드
-    public static <T> ResponseDTO<T> fail(int status, String errorMessage) {
-        return new ResponseDTO<>(false, null, status, errorMessage);
+    public static <T> ResponseDTO<T> fail(HttpStatus status, String errorMessage) {
+        return new ResponseDTO<>(false, null, status.value(), errorMessage);
     }
 
 

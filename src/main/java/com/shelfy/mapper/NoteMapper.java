d8f27aa41d195e8b0bd3a0cdd44b97aa0a04e661
ParkKyunglim <pkl1004125@gmail.com>
2025-02-06 18:33:50 +0900

    노트 북마크 기능 추가/리팩토링

--- src/main/java/com/shelfy/mapper/NoteMapper.java
+++ src/main/java/com/shelfy/mapper/NoteMapper.java
@@ -7,29 +7,32 @@ import org.apache.ibatis.annotations.*;
 import java.util.List;
 
 /*
-     날짜 : 2025/02/04
+     날짜 : 2025/02/06
      이름 : 박경림
-     내용 : NoteMapper 생성 - Create, Read, Update, Delete 추가
+     내용 :
+        - NoteMapper 생성 - Create, Read, Update, Delete 추가
+        - notePin(북마크) 기능, 쿼리 추가
 */
 
 @Mapper
 public interface NoteMapper {
 
-    // 메모 작성 (Create)
+    // 노트 글 작성 (Create)
     @Insert("INSERT INTO tb_note (note_user_id, note_title, note_contents, note_image, note_r_state_id, note_pin, note_category) " +
             "VALUES (#{noteUserId}, #{noteTitle}, #{noteContents}, #{noteImage}, #{noteRStateId}, #{notePin}, #{noteCategory})")
-    void insertNote(NoteRequestDTO noteRequestDTO); // 메모 데이터 DB에 저장
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")  // 자동 생성된 ID를 noteId에 주입
+    void insertNote(NoteRequestDTO noteRequestDTO);  // 노트 글 데이터 DB에 저장
 
-    // 모든 메모 목록 조회 (Read - List)
+    // 모든 노트 글 목록 조회 (Read - List)
     @Select("SELECT note_id AS noteId, note_title AS noteTitle, note_contents AS noteContents, " +
             "note_image AS noteImage, note_pin AS notePin, note_category AS noteCategory, " +
             "note_created_at AS noteCreatedAt, note_updated_at AS noteUpdatedAt " +
             "FROM tb_note " +
-            "WHERE note_user_id = #{noteUserId} " +  // 로그인한 유저의 메모만 조회
+            "WHERE note_user_id = #{noteUserId} " +  // 로그인한 유저의 노트 글만 조회
             "ORDER BY note_created_at DESC")
     List<NoteResponseDTO> getAllNotes(int noteUserId);
 
-    // 특정 메모 상세 조회 (Read - Detail)
+    // 특정 노트 글 상세 조회 (Read - Detail)
     @Select("SELECT note_id AS noteId, note_title AS noteTitle, note_contents AS noteContents, " +
             "note_image AS noteImage, note_pin AS notePin, note_category AS noteCategory, " +
             "note_created_at AS noteCreatedAt, note_updated_at AS noteUpdatedAt " +
@@ -38,7 +41,9 @@ public interface NoteMapper {
     NoteResponseDTO getNoteById(int noteId);
 
 
-    // 메모 부분 수정 (PATCH - 보낸 필드만 업데이트)
+    // 노트 글 부분 수정 (PATCH - 보낸 필드만 업데이트)
+    //`<script>` MyBatis에서 동적 SQL을 사용할 수 있도록 지원 `<set>`자동으로 `,`(콤마) 정리해줌
+    // `<if test='변수 != null'>` 값이 있을 때만 `UPDATE` 실행
     @Update("<script>" +
             "UPDATE tb_note " +
             "<set>" +
@@ -54,7 +59,15 @@ public interface NoteMapper {
             "</script>")
     void patchNote(NoteRequestDTO noteRequestDTO);
 
-    // 메모 삭제
+    // 노트 글 삭제
     @Delete("DELETE FROM tb_note WHERE note_id = #{noteId}")
-    void deleteNote(int noteId);    // 메모 ID로 삭제
+    void deleteNote(int noteId);    // 노트 글 ID로 삭제
+
+    // 특정 노트 글 북마크 추가/해제
+    @Update("UPDATE tb_note SET note_pin = #{notePin} WHERE note_id = #{noteId}")
+    void updateNotePin(@Param("noteId") int noteId, @Param("notePin") boolean notePin);
+
+    // 북마크된 노트 목록 조회 (notePin이 true인 경우만)
+    @Select("SELECT * FROM tb_note WHERE note_user_id = #{noteUserId} AND note_pin = true")
+    List<NoteResponseDTO> getPinnedNotes(@Param("noteUserId") int noteUserId);
 }
